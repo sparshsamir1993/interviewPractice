02f81e6212bb527898596f79d9354ad7df8580e8
sparshsamir <sparshsamir1993@gmail.com>
2020-09-06 22:10:40 -0400

    LL head and tail insert

--- LinkedList.js
+++ LinkedList.js
@@ -1,6 +1,6 @@
 class Node {
   constructor(value, next, prev) {
-    this.value = values;
+    this.value = value;
     this.next = next;
     this.prev = prev;
   }
@@ -13,9 +13,42 @@ class LinkedList {
   }
 
   addToHead(value) {
-    let node = new Node(value, null, this.head);
-    if (this.head) {
-      //   this.head.next =
+    let node = new Node(value, null, null);
+    if (!this.head && !this.next) {
+      this.head = node;
+      this.tail = node;
+      // node.next = this.tail;
+    } else {
+      node.next = this.head;
+      this.head = node;
+    }
+  }
+
+  addToTail(value) {
+    let node = new Node(value, null, null);
+    if (!this.head && !this.next) {
+      this.head = node;
+      node.next = this.tail;
+    } else {
+      if (this.tail) {
+        this.tail.next = node;
+        this.tail = node;
+      }
+    }
+  }
+
+  displayLinkedList() {
+    let start = this.head;
+    let current = start;
+    do {
+      if (current == null) {
+        return;
+      }
+      console.log(current.value + "->");
+      current = current.next;
+    } while (current && current.next != null);
+    if (current && current.next == null) {
+      console.log(current.value + "->");
     }
   }
 }
