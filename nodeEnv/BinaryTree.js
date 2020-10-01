b927d4318a917259e0a98affebc751f0d9e57253
sparshsamir <sparshsamir1993@gmail.com>
2020-10-01 16:38:30 -0400

    BST deletetion done

--- nodeEnv/BinaryTree.js
+++ nodeEnv/BinaryTree.js
@@ -41,31 +41,115 @@ class BinaryTree {
       }
     }
   }
+
+  delete(value) {
+    let foundNode = this.find(value);
+    console.log(foundNode);
+    if (foundNode) {
+      let { node, prevNode } = foundNode;
+      if (node) {
+        if (!prevNode) {
+          // for root
+          if (!node.leftChild && !node.rightChild) {
+            this.root = null;
+          }
+          if (!node.leftChild) {
+            this.root = node.rightChild;
+          } else if (!node.rightChild) {
+            this.root = node.leftChild;
+          }
+        } else {
+          // any node except root
+          if (!node.leftChild && !node.rightChild) {
+            // leaf node
+            if (prevNode.leftChild == node) {
+              console.log("left same");
+              prevNode.leftChild = undefined;
+            } else if (prevNode.rightChild == node) {
+              console.log("right same");
+              prevNode.rightChild = undefined;
+            }
+          } else if (!node.leftChild) {
+            // doesn't have left child
+            prevNode.leftChild = node.rightChild;
+          } else if (!node.rightChild) {
+            // doesn't have right child
+            prevNode.leftChild = node.leftChild;
+          } else if (node.rightChild && node.leftChild) {
+            // node with both children
+            let currentNode = node.rightChild;
+            let minNode = currentNode;
+            let prevMinNode = node;
+            // find min node in right subtree
+            while (currentNode) {
+              if (currentNode.leftChild) {
+                prevMinNode = currentNode;
+                currentNode = currentNode.leftChild;
+              } else {
+                minNode = currentNode;
+                break;
+              }
+            }
+            node.value = minNode.value;
+            if (!minNode.leftChild && !minNode.rightChild) {
+              console.log("no left no right");
+              console.log(prevMinNode);
+              console.log(minNode);
+              if (prevMinNode.leftChild == minNode) {
+                console.log("removing left");
+                prevMinNode.leftChild = undefined;
+              }
+              if (prevMinNode.rightChild == minNode) {
+                console.log("removing left");
+                prevMinNode.rightChild = undefined;
+              }
+            } else if (!minNode.leftChild) {
+              console.log("no left");
+              minNode.value = minNode.rightChild.value;
+              minNode.rightChild = undefined;
+            } else if (!minNode.rightChild) {
+              console.log(" no right");
+              minNode.value = minNode.leftChild.value;
+              minNode.leftChild = undefined;
+            }
+          }
+        }
+      }
+    }
+  }
   find(value) {
     value = +value;
+    let toReturn = { prevNode: null, node: null };
     if (this.root.value == value) {
       console.log("\n Found at ROOT ! \n");
-      return;
+      toReturn.node = this.root;
+      return toReturn;
     }
     let currentNode = this.root;
+    let previousOne;
     while (currentNode) {
       let currentValue = currentNode.value;
       if (value == currentValue) {
         console.log("\n Found ! \n");
-        return;
+        toReturn.node = currentNode;
+        toReturn.prevNode = previousOne;
+        return toReturn;
       } else if (value < currentValue) {
+        previousOne = currentNode;
         currentNode = currentNode.leftChild;
       } else if (value > currentValue) {
+        previousOne = currentNode;
         currentNode = currentNode.rightChild;
       }
     }
     console.log("Not found!");
+    return false;
   }
 }
 
 function getUserChoice() {
   let userOpChoice = prompt.question(
-    "What do you want to do ?\n1.Add node\n2.Find node\n"
+    "What do you want to do ?\n1.Add node\n2.Find node\n3.Delete node\n4.Show tree\n"
   );
 
   return userOpChoice;
@@ -84,16 +168,25 @@ exports.BinaryTree = () => {
       continue;
     }
     let userChoice = getUserChoice();
+    let string;
     switch (userChoice) {
       case "1":
         let newNum = prompt.question("What number to add ?");
         firstBT.insert(newNum);
-        let string = JSON.stringify(firstBT, null, 4);
+        string = JSON.stringify(firstBT, null, 4);
         console.log(string);
         break;
       case "2":
         let numToFind = prompt.question("What number to find ?");
         firstBT.find(numToFind);
+        break;
+      case "3":
+        let numToDel = prompt.question("What number to Delete ?");
+        firstBT.delete(numToDel);
+        break;
+      case "4":
+        string = JSON.stringify(firstBT, null, 4);
+        console.log(string);
         break;
     }
   } while (restartPrompt);
